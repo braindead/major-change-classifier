96654be5dd5a779276e6dfefe6a80bbc148c63d2
Aaron Zira <aaronzira@gmail.com>
2016-10-20 19:40:32 -0700

    touch ups

--- checker.py
+++ checker.py
@@ -8,10 +8,11 @@ try:
         checker.train()
     elif sys.argv[2] == 'predict':
         checker.predict(sys.argv[3])
-    elif sys.argv[2] == 'check':
-        checker.check_results()
     else:
-        print('Invalid second argument. Expected format is <PATH TO DATASET AND MODEL> <'train' OR 'predict' OR 'check'> <PATH TO CSV FILE (if 'predict')>')
+        print("Invalid second argument. Expected format is 
+              <PATH TO DATASET AND MODEL> <'train' OR 'predict'> 
+              <PATH TO CSV FILE (if 'predict')>")
 
 except IndexError:
-    print("Required arguments are <PATH TO DATASET AND MODEL> <'train' OR 'predict' OR 'check'> <PATH TO CSV FILE (if 'predict')>")
+    print("Required arguments are <PATH TO DATASET AND MODEL> 
+          <'train' OR 'predict'> <PATH TO CSV FILE (if 'predict')>")
