7d52c853c6b24fa80c8c3f18ed877e31d2e78350
Aaron Zira <aaronzira@gmail.com>
2016-10-26 10:46:22 -0700

    Update checker.py

--- checker.py
+++ checker.py
@@ -2,17 +2,14 @@ import classifier
 import sys
 
 try:
-    checker = classifiertest.error_checker(sys.argv[1])
+    checker = classifier.error_checker(sys.argv[1])
 
     if sys.argv[2] == 'train':
         checker.train()
     elif sys.argv[2] == 'predict':
         checker.predict(sys.argv[3])
     else:
-        print("Invalid second argument. Expected format is 
-              <PATH TO DATASET AND MODEL> <'train' OR 'predict'> 
-              <PATH TO CSV FILE (if 'predict')>")
+        print "Invalid second argument. Expected format is <PATH TO DATASET AND MODEL> <'train' OR 'predict'> <PATH TO CSV FILE (if 'predict')>"
 
 except IndexError:
-    print("Required arguments are <PATH TO DATASET AND MODEL> 
-          <'train' OR 'predict'> <PATH TO CSV FILE (if 'predict')>")
+    print "Required arguments are <PATH TO DATASET AND MODEL> <'train' OR 'predict'> <PATH TO CSV FILE (if 'predict')>"
