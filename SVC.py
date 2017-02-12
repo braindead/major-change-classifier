953fa2beeb0d98eb9cbd423bec126fce6582e655
Rajiv Poddar <rajiv@scribie.com>
2017-02-12 18:01:20 +0530

    Pedi

--- SVC.py
+++ SVC.py
@@ -4759,6 +4759,7 @@ class Checker():
             #text = re.sub("(\d+)(th)","\g<1>",text)
 
             # substitutions 
+            text = re.sub("\\bpaedi", "pedi", text);
             text = re.sub("\\bpyjama", "pajama", text);
             text = re.sub("\\bmum", "mom", text);
             text = re.sub("\\bcheque", "check", text);
