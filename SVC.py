d8df8bcdfb7c14f2844b6f3b7c26260428c2a3d8
Rajiv Poddar <rajiv@scribie.com>
2017-02-13 00:38:12 +0530

    storey              story

--- SVC.py
+++ SVC.py
@@ -4769,6 +4769,7 @@ class Checker():
             #text = re.sub("(\d+)(th)","\g<1>",text)
 
             # substitutions 
+            text = re.sub("\\bstorey\\b", "story", text);
             text = re.sub("\\bnosey\\b", "nosy", text);
             text = re.sub("\\bspelt\\b", "spelled", text);
             text = re.sub("\\bdreamt\\b", "dreamed", text);
