6450b4a5b5856f9edabe88060504a1b4a95cd161
Rajiv Poddar <rajiv@scribie.com>
2017-02-18 11:47:17 +0530

    five,v

--- SVC.py
+++ SVC.py
@@ -3611,6 +3611,7 @@ class Checker():
 
         # known minors
         self.known_minors = [
+                "five,v",
                 "usd,us",
                 "shall,should",
                 "sats,sat",
